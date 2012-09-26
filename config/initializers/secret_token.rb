d8c002f9d513801c296d5c8e8c653f111ba63ed2
Ivar Vong <ivarjvong@gmail.com>
2012-09-26 14:43:06 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Emeraldevents::Application.config.secret_token = 'dea2c3d53a029b141ed20453c8ccb23d92557ed8fcd6329cf6c0119b56c3f5651addec89acfaeea6129a24b2784f7d019358bb8963e455973cbd077aacff104e'
